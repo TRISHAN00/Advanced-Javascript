68773033d93bf777b978fbe51e10de16e3dc3bac
trishan <trishansaha43@gmail.com>
2021-02-19 02:57:55 -0800

    arrow practice

--- arrow-function.js
+++ arrow-function.js
@@ -1,9 +1,54 @@
-var javaScript = {
-    name : 'JavaScript',
-    libraries : ['React', 'Angular', 'Vue'],
-    printLibraries : function () {
-        this.libraries.forEach((a) => console.log(`${this.name} loves ${a}`))
-    },
-}
+// var javaScript = {
+//     name : 'JavaScript',
+//     libraries : ['React', 'Angular', 'Vue'],
+//     printLibraries : function () {
+//         this.libraries.forEach((a) => console.log(`${this.name} loves ${a}`))
+//     },
+// }
 
-javaScript.printLibraries()
+// javaScript.printLibraries()
+
+
+
+
+// let loves = {
+//     name : 'I',
+//     country: ['Bangladesh', 'Australia', 'India', 'USA'],
+//     printCountry : function () {
+//         this.country.forEach((countries) => console.log(`${this.name} loves ${countries}`));
+//     }
+// }
+
+// loves.printCountry()
+
+
+
+// let studentsDetails = {
+//     name : ['Tirhsn', 'Rodoushi', 'Riya', 'Oushi', 'Mithun'],
+//     numbers : [80, 50, 60, 40, 50],
+//     details : function () {
+//         console.log(this);
+//         this.name.forEach((names) => {
+//             console.log(`${this.name[2]} is got ${this.numbers[1]}`);
+//         })
+//     }
+// }
+
+// studentsDetails.details()
+
+
+
+
+// function number(a, b) {
+//     return a + b
+// }
+
+// console.log(number(10, 20));
+
+
+// when i going to make as below to arrow function
+
+// let number = (a, b) => a + b 
+
+
+// console.log(number(10, 25));
